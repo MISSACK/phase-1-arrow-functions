0c51f9382b3cb42ef41f3217afaf6f5a2126f14c
Musa Issack <musa.m.issack@gmail.com>
2023-06-18 11:04:26 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function(a, b) {
+    return 2000 / 100;
+};
+
+const square = (num) => {
+    return num * num;
+};
+
+const add = (a, b) => {
+    return a + b;
+};
+  
+  
+
+  
+
+  
+  
